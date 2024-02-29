fb7e608039a937592616d4a783b89d134d355006
Logan Martin <71342159+Logan-Martin@users.noreply.github.com>
2024-02-29 09:48:35 -0500

    Update JustLCD_Code.py

--- Our Physical Counter/Code/JustLCD_Code.py
+++ Our Physical Counter/Code/JustLCD_Code.py
@@ -13,21 +13,9 @@ from CircuitPython_LCD.lcd.i2c_pcf8574_interface import I2CPCF8574Interface  # t
 # Make sure to have at least 5v for LCD. Use battery pack or use VBUS. Note: VBUS has no volts when the pico's USB is unplugged.
 
 i2c_address = 0x27 # check the i2c address of your specific device
-cols = 16
-rows = 2
 i2c_bus_0 = busio.I2C(board.GP11, board.GP10) # make sure to check (SLC, SCA)
 interface = I2CPCF8574Interface(i2c_bus_0, i2c_address)
-lcd = LCD(interface, num_rows=rows, num_cols=cols)
+lcd = LCD(interface, num_rows=2, num_cols=16) # change the num based on that LCD's rows and cols
 
-
-lcd.print("abc ")
-lcd.print("This is quite long and will wrap onto the next line automatically.")
-
-lcd.clear()
-
-# Start at the second line, fifth column (numbering from zero).
-lcd.set_cursor_pos(1, 4)
+lcd.clear() # do this each time you want to write something new on the LCD, if you don't text will overlap
 lcd.print("Here I am")
-
-# Make the cursor visible as a line.
-lcd.set_cursor_mode(CursorMode.LINE)
